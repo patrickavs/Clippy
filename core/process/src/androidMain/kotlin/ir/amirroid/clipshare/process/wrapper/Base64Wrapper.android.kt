f4d56ac5ffc80f1693e90f79e969fc1b3545de12
Amirreza Gholami <jv233114@gmail.com>
2025-09-13 16:41:26 +0330

    fix: fix base64 encoder

--- core/process/src/androidMain/kotlin/ir/amirroid/clipshare/process/wrapper/Base64Wrapper.android.kt
+++ core/process/src/androidMain/kotlin/ir/amirroid/clipshare/process/wrapper/Base64Wrapper.android.kt
@@ -3,11 +3,9 @@ package ir.amirroid.clipshare.process.wrapper
 import android.util.Base64
 
 actual object Base64Wrapper {
-    actual fun encodeToString(bytes: ByteArray): String {
-        return Base64.encodeToString(bytes, Base64.DEFAULT)
-    }
+    actual fun encodeToString(bytes: ByteArray): String =
+        Base64.encodeToString(bytes, Base64.NO_WRAP)
 
-    actual fun decodeToByteArray(value: String): ByteArray {
-        return Base64.decode(value, Base64.DEFAULT)
-    }
+    actual fun decodeToByteArray(value: String): ByteArray =
+        Base64.decode(value, Base64.DEFAULT)
 }
